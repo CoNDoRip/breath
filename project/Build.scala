708485fcfb7471ece668a50edcd3a59ccdaa4a5e
Idel <cndr.ip@gmail.com>
2013-06-05 23:14:58 +0400

    Configurate connection to Postgres, first page and add favicon

--- project/Build.scala
+++ project/Build.scala
@@ -11,11 +11,13 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     javaCore,
     javaJdbc,
-    javaEbean
+    javaJpa,
+    "org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
+      ebeanEnabled := false
   )
 
 }
