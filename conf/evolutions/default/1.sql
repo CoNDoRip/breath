8d3273154bafe54084f8daaeb13cc5855169a776
Idel <cndr.ip@gmail.com>
2013-06-21 01:58:32 +0600

    Add 'Level' entity to DB and models

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,34 +2,50 @@
 
 # --- !Ups
 
+-------------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS Level (
+    id             INT             PRIMARY KEY
+  , name           VARCHAR(20)     UNIQUE
+  , image          VARCHAR(25)     UNIQUE
+);
+
+
+-------------------------------------------------------------------------------
 CREATE SEQUENCE Profile_seq START WITH 1;
 
-CREATE TABLE Profile (
-   id             NUMERIC         PRIMARY KEY    DEFAULT nextval('Profile_seq')
-  ,email		      VARCHAR(30)     UNIQUE
-  ,password       CHAR(40)        NOT NULL       --hashsum of password
-  ,username       VARCHAR(30)                    --nick name or login
-  ,first_name     VARCHAR(30)     
-  ,last_name      VARCHAR(30)     
-  ,birthday       DATE
-  ,gender         CHAR(1)
-  ,level          INT             NOT NULL DEFAULT 1
-  ,points         INT             NOT NULL DEFAULT 0
-  ,completed      INT             NOT NULL DEFAULT 0
-  ,todo_list      INT             NOT NULL DEFAULT 0
-  ,status         VARCHAR(200)    DEFAULT 'I like real life!'
+CREATE TABLE IF NOT EXISTS Profile (
+    id             NUMERIC         PRIMARY KEY   DEFAULT nextval('Profile_seq')
+  , email		       VARCHAR(30)     UNIQUE
+  , password       CHAR(40)        NOT NULL      --hashsum of password
+  , username       VARCHAR(30)                   --nick name or login
+  , first_name     VARCHAR(30)     
+  , last_name      VARCHAR(30)     
+  , birthday       DATE
+  , gender         CHAR(1)
+  , level          INT             NOT NULL      DEFAULT 1
+  , points         INT             NOT NULL      DEFAULT 0
+  , completed      INT             NOT NULL      DEFAULT 0
+  , todo_list      INT             NOT NULL      DEFAULT 0
+  , status         VARCHAR(200)                  DEFAULT 'I like real life!'
+  , avatar         VARCHAR(30)
 
-  ,CONSTRAINT User_gender CHECK (gender = 'M' or gender = 'F')
-  ,CONSTRAINT User_greather_then_zero CHECK (level > 0 and points >= 0 and completed >= 0 and todo_list >= 0)
+  , CONSTRAINT Profile_Level FOREIGN KEY (level) REFERENCES Level(id)
+  , CONSTRAINT Profile_gender CHECK (gender = 'M' or gender = 'F')
+  , CONSTRAINT Profile_greather_then_zero CHECK (level > 0
+                                            and points >= 0 
+                                            and completed >= 0 
+                                            and todo_list >= 0)
 );
 
 ALTER SEQUENCE Profile_seq OWNED BY Profile.id;
 
-CREATE INDEX ix_profile_email   ON Profile(email);
+
+-------------------------------------------------------------------------------
 
 
 # --- !Downs
 
-DROP TABLE IF EXISTS Profile;
-
+DROP TABLE    IF EXISTS Profile;
 DROP SEQUENCE IF EXISTS Profile_seq;
+
+DROP TABLE    IF EXISTS Level;
--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,5 +1,20 @@
 # --- !Ups
 
+INSERT INTO Level VALUES (1,  'Newbie',              'Newbie.gif');
+INSERT INTO Level VALUES (2,  'Pupil',               'Pupil.png');
+INSERT INTO Level VALUES (3,  'Teen',                'Teen.png');
+INSERT INTO Level VALUES (4,  'Adult',               'Adult.png');
+INSERT INTO Level VALUES (5,  'Expert',              'Expert.png');
+INSERT INTO Level VALUES (6,  'Master',              'Master.png');
+INSERT INTO Level VALUES (7,  'Professional',        'Professional.png');
+INSERT INTO Level VALUES (8,  'Leader',              'Leader.png');
+INSERT INTO Level VALUES (9,  'Guru',                'Guru.png');
+INSERT INTO Level VALUES (10, 'Idol',                'Idol.png');
+INSERT INTO Level VALUES (11, 'Higher Intelligence', 'HigherIntelligence.png');
+INSERT INTO Level VALUES (12, 'God',                 'God.png');
+
+
+
 INSERT INTO Profile (email, password, first_name, last_name, gender) 
           VALUES ('cndr.ip@gmail.com', '0aa371f7f51bd1312cef02e827f35122c46aa011', 'Idel', 'Pivnitskiy', 'M');
 INSERT INTO Profile (email, password, first_name, last_name, gender) 
@@ -11,3 +26,5 @@ INSERT INTO Profile (email, password, first_name, last_name, gender)
 # --- !Downs
 
 DELETE FROM Profile;
+
+DELETE FROM Level;
