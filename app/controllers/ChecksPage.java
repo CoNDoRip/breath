e96abff5b4859bde9bcf0ae33aeb122ba8bf6f5c
Idel <cndr.ip@gmail.com>
2013-06-23 21:08:12 +0600

    Add API v1 function for getting unckecked user tasks for current user

---
+++ app/controllers/ChecksPage.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import play.db.jpa.*;
+
+import views.html.*;
+import models.Profile;
+import models.Checks;
+import models.UserTask;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ObjectNode;
+import play.libs.Json;
+
+import java.util.List;
+
+@Security.Authenticated(Secured.class)
+public class ChecksPage extends Controller {
+
+	/**
+	* Get user's avaliable tasks
+	*/
+	@Transactional(readOnly=true)
+	public static Result getChecks(Integer page) {
+		if (page > 0) {
+			String id = session("id");
+			Long profileId = Long.valueOf(id).longValue();
+			Profile profile = Profile.findById(profileId);
+
+			List<UserTask> listOfChecks = Checks.findChecks(profileId, page);
+			
+			return ok(Json.toJson(listOfChecks));
+		} else {
+			return Application.errorResponse("Error in page number! Page must be greater than 0");
+		}
+	}
+
+}
